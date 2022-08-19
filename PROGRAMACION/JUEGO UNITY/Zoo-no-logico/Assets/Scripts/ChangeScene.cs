52e99de1b6c39636edea290b02f5840bfd3bcce7
Delri16 <zantyyyy@gmail.com>
2022-08-19 16:03:21 -0300

    Última prueba del día. A ver si funciona todo bien
    Cambie las 2 escenas

---
+++ PROGRAMACION/JUEGO UNITY/Zoo-no-logico/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public void MoveToScene(int sceneID)
+    {
+        SceneManager.LoadScene(sceneID);
+    }
+}
