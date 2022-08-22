b1b4f9c92f85ac39539a6025c9ae5480bc74332a
Pezzi <agus.pezzim@gmail.com>
2022-08-22 17:17:02 -0300

    implementacion basica de sistema de inventario

---
+++ PROGRAMACION/JUEGO UNITY/Zoo-no-logico/Assets/Scripts/AnimalCombination.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalCombination : MonoBehaviour {
+
+    [SerializeField] private UI_Inventory uiInventory;
+
+    private Inventory inventory;
+
+    private void Awake()
+    {
+        inventory = new Inventory();
+
+        uiInventory.SetInventory(inventory);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ PROGRAMACION/JUEGO UNITY/Zoo-no-logico/Assets/Scripts/UI_Inventory.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Inventory : MonoBehaviour {
+
+    private Inventory inventory;
+    private Transform itemSlotContainer;
+    private Transform itemSlotTemplate;
+
+    public void Awake()
+    {
+        itemSlotContainer = transform.Find("itemSlotContainer");
+        itemSlotTemplate = itemSlotContainer.Find("itemSlotTemplate");
+    }
+
+    public void SetInventory(Inventory inventory)
+    {
+        this.inventory = inventory;
+        RefreshInventoryItems();
+    }
+
+    public void RefreshInventoryItems()
+    {
+        foreach (Item item in inventory.GetItemList())
+        {
+            int x = 0;
+            int y = 0;
+            float itemSlotCellSize = 30f;
+            RectTransform itemSlotRectTransform = Instantiate(itemSlotTemplate, itemSlotContainer).GetComponent<RectTransform>();
+            itemSlotRectTransform.gameObject.SetActive(true);
+            itemSlotRectTransform.anchoredPosition = new Vector2(x * itemSlotCellSize, y * itemSlotCellSize);
+            x++;
+            if (x > 4)
+            {
+                x = 0;
+                y++;
+            }
+        }
+    }
+}
