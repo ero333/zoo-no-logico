0d32140b6d4896c5fd12b6e8c6095c61da34e770
Delri16 <zantyyyy@gmail.com>
2022-08-24 22:03:14 -0300

    Inventario programado (falta el arte)

---
+++ PROGRAMACION/JUEGO UNITY/Zoo-no-logico/Assets/Scripts/Inventory_test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory_test : MonoBehaviour {
+
+    
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Inventory test");
+    }
+
+    public void PrintAnimal(string animal)
+    {
+        Debug.Log(animal);
+    }
+
+}
