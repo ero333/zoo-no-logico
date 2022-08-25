b2597c967c559a45abd65b7d3d5ab5c9f1c6dcaa
Delri16 <zantyyyy@gmail.com>
2022-08-25 12:24:55 -0300

    Cambios en la combinacion de los animales

---
+++ PROGRAMACION/JUEGO UNITY/Zoo-no-logico/Assets/Scripts/RawImageTexture.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RawImageTexture : MonoBehaviour
+{
+    public Image original;
+    public Sprite newSprite;
+
+    void Start()
+    {
+    }
+
+    public void NewImage()
+    {
+        Debug.Log('a');
+        original.sprite = newSprite;
+    }
+}
