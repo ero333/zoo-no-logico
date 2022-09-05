f8853ab7ed6b12e7dbbf74cd8953b2b042ea7c63
Agos-123 <a.torreblancabaier@usal.edu.ar>
2022-09-04 21:25:19 -0300

    +Prueba de selección de jaulas

---
+++ PROGRAMACION/PRUEBAS/Prueba selección de jaula/Assets/Precio.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Precio : MonoBehaviour {
+
+
+
+    public Text textoPrecio;
+    public string Jaula;
+    public int precio;
+
+
+    // Use this for initialization
+    void Start () {
+        textoPrecio = GameObject.FindGameObjectWithTag("PrecioJaulas").GetComponent<Text>();
+
+        if (Jaula == "JC")
+        {
+            precio = 500;
+            textoPrecio.text = precio.ToString();
+        }
+        if (Jaula == "JM")
+        {
+            precio = 1500;
+            textoPrecio.text = precio.ToString();
+        }
+        if (Jaula == "JG")
+        {
+            precio = 2500;
+            textoPrecio.text = precio.ToString();
+        }
+
+        {
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
