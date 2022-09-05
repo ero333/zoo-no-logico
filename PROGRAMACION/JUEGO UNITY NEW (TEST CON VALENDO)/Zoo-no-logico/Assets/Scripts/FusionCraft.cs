8a557255182f1a27b174da7e723554b8fdf3d833
Canade Maca <63354138+canademaca@users.noreply.github.com>
2022-09-05 19:06:34 -0300

    crafteo/popularidad/peligrosidad scripts
     (COMENTADOS)

---
+++ PROGRAMACION/JUEGO UNITY NEW (TEST CON VALENDO)/Zoo-no-logico/Assets/Scripts/FusionCraft.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// public class FusionCraft : MonoBehaviour
+// {
+//         public BaseAnimals slot1;
+//         public BaseAnimals slot2;
+//         public BaseAnimals slot3;
+
+//         public bool fused;
+//         private Rigidbody2D rigidBody;
+//         private GameObject fusion;
+//         private GameObject failedfusion;
+
+
+//          private void Start()
+//     {
+//         rigidBody = GetComponent<Rigidbody2D>();
+//         fusion = gameObject.GetComponent<TERMINADOS>();
+//         failedfusion = gameObject.GetComponent<TERMINADOS>();
+//     }
+//         public InventarioUsuario(BaseAnimals s1, BaseAnimals s2, BaseAnimals s3)
+//         {
+//             if((s1 == database.Carpincho)||(s2 == database.Cocodrilo)||(s3 == database.Murcielago))
+//             {
+//                 fused = true;
+//                 fusion;//= imagen de resultado//
+//             }
+//             if((s1 == database.Cocodrilo)||(s2 == database.Carpincho)||(s3 == database.Murcielago))
+//             {
+//                 fused = true;
+//                 fusion;//= imagen de resultado//
+//             }
+//             if((s1 == database.Murcielago)||(s2 == database.Carpincho)||(s3 == database.Cocodrilo))
+//             {
+//                 fused = true;
+//                 fusion;//= imagen de resultado//
+//             }
+//             if((s1 == database.Carpincho)||(s2 == database.Cocodrilo)||(s3 == database.Murcielago))
+//             {
+//                 fused = false;
+//                 failedfusion;//= imagen de resultado//MASAAMORFA
+//             }
+//             if((s1 == database.Cocodrilo)||(s2 == database.Carpincho)||(s3 == database.Murcielago))
+//             {
+//                 fused = false;
+//                 failedfusion;//= imagen de resultado//MASAAMORFA
+//             }
+//             if((s1 == database.Murcielago)||(s2 == database.Carpincho)||(s3 == database.Cocodrilo))
+//             {
+//                 fused = true;
+//                 fusion;//= imagen de resultado//
+//             }
+//             if((s1 == database.Carpincho)||(s2 == database.Murcielago)||(s3 == database.Cocodrilo))
+//             {
+//                 fused = true;
+//                 fusion;//= imagen de resultado//
+//             }
+//         }
+
+//         // public InventarioUsuario(BaseAnimals new) : this(fusion)
+//         // {
+
+//         // }
+// }
