bc170ece7c2366bc05e7bc4be7b3252f54edd6a0
Agos-123 <a.torreblancabaier@usal.edu.ar>
2022-09-07 17:41:36 -0300

    +Backup Unity

---
+++ PROGRAMACION/JUEGO UNITY NEW (TEST CON VALENDO) - Backup/Zoo-no-logico/Assets/Items & Inventory/Scripts/SlotDeCruza.cs
@@ -0,0 +1,20 @@
+﻿public class SlotDeCruza : ItemSlot
+{
+
+    public EquipmentType EquipmentType;
+
+    protected override void OnValidate()
+    {
+        base.OnValidate();
+        gameObject.name = EquipmentType.ToString() + "Slot";
+    }
+
+    public override bool CanReceiveItem(Item item)
+    {
+        if (item == null)
+            return true;
+
+        EquippableItem equippableItem = item as EquippableItem;
+        return equippableItem != null && equippableItem.EquipmentType == EquipmentType;
+    }
+}
---
+++ PROGRAMACION/JUEGO UNITY NEW (TEST CON VALENDO) - Backup/Zoo-no-logico/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+
+    void Start()
+    {
+    }
+
+
+    public void MoveToScene(int sceneID)
+    {
+        Debug.Log("Cambio de escena");
+        SceneManager.LoadScene(sceneID);
+    }
+
+    
+}
