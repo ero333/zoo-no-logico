5164a8917bef9c6621dbf0fe61adbea768a827c6
Delri16 <zantyyyy@gmail.com>
2022-09-08 14:07:40 -0300

    Sistema de porcentaje

---
+++ PROGRAMACION/ESTE ES EL QUE HAY QUE EDITAR/Zoo-no-logico/Assets/Scripts/CombinateByPercent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CombinateByPercent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MoveToCombinationScene()
+    {
+        if (PlayerPrefs.GetFloat("Porcentaje")>49)
+        {
+            SceneManager.LoadScene(6);
+        }
+        else
+        {
+            SceneManager.LoadScene(9);
+        }
+    }
+}
