9acd0234b7bf4c41f4cff6cf714c699184a2d1fc
MartinIpolito <martinidelcio.ipolito@usal.edu.ar>
2022-09-26 20:01:43 -0300

    Perdiste

---
+++ PROGRAMACION/ESTE ES EL QUE HAY QUE EDITAR/Zoo-no-logico/Assets/Scripts/TogglePerdida.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TogglePerdida : MonoBehaviour {
+
+	public GameObject Perdiste;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void click () {
+		if (Perdiste.activeInHierarchy == true)
+		Perdiste.SetActive(false);
+		else
+		Perdiste.SetActive(true);
+	}
+}
