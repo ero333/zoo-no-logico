01c99525511aaa45ac2039f58c16f5d593ffd588
Agos-123 <a.torreblancabaier@usal.edu.ar>
2022-09-29 22:09:08 -0300

    Pantalla de Cartas programada

---
+++ PROGRAMACION/ESTE ES EL QUE HAY QUE EDITAR/Zoo-no-logico/Assets/Scripts/AbrirCartas.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbrirCartas : MonoBehaviour {
+
+    public GameObject CartaInfo;
+    public GameObject Sombreado;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+   public void Abrir()
+    {
+        CartaInfo.SetActive(true);
+        Sombreado.SetActive(true);
+    }
+
+
+    public void Cerrar()
+    {
+        CartaInfo.SetActive(false);
+        Sombreado.SetActive(false);
+    }
+}
