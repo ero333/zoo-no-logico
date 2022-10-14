71d63dac4f22046c1eac05c7693572a85e1abc8e
Agos-123 <a.torreblancabaier@usal.edu.ar>
2022-10-14 01:21:44 -0300

    +Pantalla tutorial y arreglitos

---
+++ PROGRAMACION/ESTE ES EL QUE HAY QUE EDITAR/Zoo-no-logico/Assets/Scripts/PasarPantallaTuto.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PasarPantallaTuto : MonoBehaviour {
+
+
+    public GameObject Pantalla;
+    public GameObject Pantalla2;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PasarPantalla()
+    {
+        Pantalla.SetActive(false);
+        Pantalla2.SetActive(true);
+    }
+
+    public void VolverPantalla()
+    {
+        Pantalla.SetActive(true);
+        Pantalla2.SetActive(false);
+    }
+}
