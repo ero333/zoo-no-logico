a497a58f0acb3e775ed4c7568412d0773404fbe4
Delri16 <zantyyyy@gmail.com>
2022-10-17 00:02:24 -0300

    Teclas para subir y bajar popularidad y monedas

---
+++ PROGRAMACION/ESTE ES EL QUE HAY QUE EDITAR/Zoo-no-logico/Assets/Scripts/CheatCodes.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheatCodes : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("p"))
+        {
+            PlayerPrefs.SetInt("Popularidad", PlayerPrefs.GetInt("Popularidad") + 5);
+        }
+
+        if (Input.GetKeyDown("o")){
+            PlayerPrefs.SetInt("Popularidad", PlayerPrefs.GetInt("Popularidad") -5);
+        }
+
+        if (Input.GetKeyDown("m"))
+        {
+            PlayerPrefs.SetInt("Moneditas", PlayerPrefs.GetInt("Moneditas") + 10000);
+        }
+
+        if (Input.GetKeyDown("n"))
+        {
+            PlayerPrefs.SetInt("Moneditas", PlayerPrefs.GetInt("Moneditas") - 10000);
+        }
+
+    }
+}
