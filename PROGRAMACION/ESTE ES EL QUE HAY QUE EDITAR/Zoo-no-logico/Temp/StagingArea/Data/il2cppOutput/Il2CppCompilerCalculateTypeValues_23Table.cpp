fcb1c671fd4930a3b2a7ea497ef93995f428dc88
Delri16 <zantyyyy@gmail.com>
2022-10-17 00:12:17 -0300

    Build!

---
+++ PROGRAMACION/ESTE ES EL QUE HAY QUE EDITAR/Zoo-no-logico/Temp/StagingArea/Data/il2cppOutput/Il2CppCompilerCalculateTypeValues_23Table.cpp
@@ -0,0 +1,380 @@
+﻿#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include <cstring>
+#include <string.h>
+#include <stdio.h>
+#include <cmath>
+#include <limits>
+#include <assert.h>
+#include <stdint.h>
+
+#include "il2cpp-class-internals.h"
+#include "codegen/il2cpp-codegen.h"
+#include "il2cpp-object-internals.h"
+
+
+// System.String
+struct String_t;
+// System.Void
+struct Void_t1185182177;
+// UnityEngine.GameObject
+struct GameObject_t1113636619;
+// UnityEngine.UI.Button
+struct Button_t4055032469;
+
+
+
+
+#ifndef RUNTIMEOBJECT_H
+#define RUNTIMEOBJECT_H
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+
+// System.Object
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+#endif // RUNTIMEOBJECT_H
+#ifndef VALUETYPE_T3640485471_H
+#define VALUETYPE_T3640485471_H
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+
+// System.ValueType
+struct  ValueType_t3640485471  : public RuntimeObject
+{
+public:
+
+public:
+};
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+// Native definition for P/Invoke marshalling of System.ValueType
+struct ValueType_t3640485471_marshaled_pinvoke
+{
+};
+// Native definition for COM marshalling of System.ValueType
+struct ValueType_t3640485471_marshaled_com
+{
+};
+#endif // VALUETYPE_T3640485471_H
+#ifndef INTPTR_T_H
+#define INTPTR_T_H
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+
+// System.IntPtr
+struct  IntPtr_t 
+{
+public:
+	// System.Void* System.IntPtr::m_value
+	void* ___m_value_0;
+
+public:
+	inline static int32_t get_offset_of_m_value_0() { return static_cast<int32_t>(offsetof(IntPtr_t, ___m_value_0)); }
+	inline void* get_m_value_0() const { return ___m_value_0; }
+	inline void** get_address_of_m_value_0() { return &___m_value_0; }
+	inline void set_m_value_0(void* value)
+	{
+		___m_value_0 = value;
+	}
+};
+
+struct IntPtr_t_StaticFields
+{
+public:
+	// System.IntPtr System.IntPtr::Zero
+	intptr_t ___Zero_1;
+
+public:
+	inline static int32_t get_offset_of_Zero_1() { return static_cast<int32_t>(offsetof(IntPtr_t_StaticFields, ___Zero_1)); }
+	inline intptr_t get_Zero_1() const { return ___Zero_1; }
+	inline intptr_t* get_address_of_Zero_1() { return &___Zero_1; }
+	inline void set_Zero_1(intptr_t value)
+	{
+		___Zero_1 = value;
+	}
+};
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+#endif // INTPTR_T_H
+#ifndef OBJECT_T631007953_H
+#define OBJECT_T631007953_H
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+
+// UnityEngine.Object
+struct  Object_t631007953  : public RuntimeObject
+{
+public:
+	// System.IntPtr UnityEngine.Object::m_CachedPtr
+	intptr_t ___m_CachedPtr_0;
+
+public:
+	inline static int32_t get_offset_of_m_CachedPtr_0() { return static_cast<int32_t>(offsetof(Object_t631007953, ___m_CachedPtr_0)); }
+	inline intptr_t get_m_CachedPtr_0() const { return ___m_CachedPtr_0; }
+	inline intptr_t* get_address_of_m_CachedPtr_0() { return &___m_CachedPtr_0; }
+	inline void set_m_CachedPtr_0(intptr_t value)
+	{
+		___m_CachedPtr_0 = value;
+	}
+};
+
+struct Object_t631007953_StaticFields
+{
+public:
+	// System.Int32 UnityEngine.Object::OffsetOfInstanceIDInCPlusPlusObject
+	int32_t ___OffsetOfInstanceIDInCPlusPlusObject_1;
+
+public:
+	inline static int32_t get_offset_of_OffsetOfInstanceIDInCPlusPlusObject_1() { return static_cast<int32_t>(offsetof(Object_t631007953_StaticFields, ___OffsetOfInstanceIDInCPlusPlusObject_1)); }
+	inline int32_t get_OffsetOfInstanceIDInCPlusPlusObject_1() const { return ___OffsetOfInstanceIDInCPlusPlusObject_1; }
+	inline int32_t* get_address_of_OffsetOfInstanceIDInCPlusPlusObject_1() { return &___OffsetOfInstanceIDInCPlusPlusObject_1; }
+	inline void set_OffsetOfInstanceIDInCPlusPlusObject_1(int32_t value)
+	{
+		___OffsetOfInstanceIDInCPlusPlusObject_1 = value;
+	}
+};
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+// Native definition for P/Invoke marshalling of UnityEngine.Object
+struct Object_t631007953_marshaled_pinvoke
+{
+	intptr_t ___m_CachedPtr_0;
+};
+// Native definition for COM marshalling of UnityEngine.Object
+struct Object_t631007953_marshaled_com
+{
+	intptr_t ___m_CachedPtr_0;
+};
+#endif // OBJECT_T631007953_H
+#ifndef COMPONENT_T1923634451_H
+#define COMPONENT_T1923634451_H
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+
+// UnityEngine.Component
+struct  Component_t1923634451  : public Object_t631007953
+{
+public:
+
+public:
+};
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+#endif // COMPONENT_T1923634451_H
+#ifndef BEHAVIOUR_T1437897464_H
+#define BEHAVIOUR_T1437897464_H
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+
+// UnityEngine.Behaviour
+struct  Behaviour_t1437897464  : public Component_t1923634451
+{
+public:
+
+public:
+};
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+#endif // BEHAVIOUR_T1437897464_H
+#ifndef MONOBEHAVIOUR_T3962482529_H
+#define MONOBEHAVIOUR_T3962482529_H
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+
+// UnityEngine.MonoBehaviour
+struct  MonoBehaviour_t3962482529  : public Behaviour_t1437897464
+{
+public:
+
+public:
+};
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+#endif // MONOBEHAVIOUR_T3962482529_H
+#ifndef UNLOCKSTOREANIMALS_T1320687291_H
+#define UNLOCKSTOREANIMALS_T1320687291_H
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+
+// UnlockStoreAnimals
+struct  UnlockStoreAnimals_t1320687291  : public MonoBehaviour_t3962482529
+{
+public:
+	// UnityEngine.UI.Button UnlockStoreAnimals::aveButton
+	Button_t4055032469 * ___aveButton_4;
+	// UnityEngine.UI.Button UnlockStoreAnimals::aranaButton
+	Button_t4055032469 * ___aranaButton_5;
+	// UnityEngine.UI.Button UnlockStoreAnimals::serpienteButton
+	Button_t4055032469 * ___serpienteButton_6;
+	// UnityEngine.GameObject UnlockStoreAnimals::aveImg
+	GameObject_t1113636619 * ___aveImg_7;
+	// UnityEngine.GameObject UnlockStoreAnimals::aranaImg
+	GameObject_t1113636619 * ___aranaImg_8;
+	// UnityEngine.GameObject UnlockStoreAnimals::serpienteImg
+	GameObject_t1113636619 * ___serpienteImg_9;
+	// UnityEngine.GameObject UnlockStoreAnimals::aveMiniImg
+	GameObject_t1113636619 * ___aveMiniImg_10;
+	// UnityEngine.GameObject UnlockStoreAnimals::aranaMiniImg
+	GameObject_t1113636619 * ___aranaMiniImg_11;
+	// UnityEngine.GameObject UnlockStoreAnimals::serpienteMiniImg
+	GameObject_t1113636619 * ___serpienteMiniImg_12;
+
+public:
+	inline static int32_t get_offset_of_aveButton_4() { return static_cast<int32_t>(offsetof(UnlockStoreAnimals_t1320687291, ___aveButton_4)); }
+	inline Button_t4055032469 * get_aveButton_4() const { return ___aveButton_4; }
+	inline Button_t4055032469 ** get_address_of_aveButton_4() { return &___aveButton_4; }
+	inline void set_aveButton_4(Button_t4055032469 * value)
+	{
+		___aveButton_4 = value;
+		Il2CppCodeGenWriteBarrier((&___aveButton_4), value);
+	}
+
+	inline static int32_t get_offset_of_aranaButton_5() { return static_cast<int32_t>(offsetof(UnlockStoreAnimals_t1320687291, ___aranaButton_5)); }
+	inline Button_t4055032469 * get_aranaButton_5() const { return ___aranaButton_5; }
+	inline Button_t4055032469 ** get_address_of_aranaButton_5() { return &___aranaButton_5; }
+	inline void set_aranaButton_5(Button_t4055032469 * value)
+	{
+		___aranaButton_5 = value;
+		Il2CppCodeGenWriteBarrier((&___aranaButton_5), value);
+	}
+
+	inline static int32_t get_offset_of_serpienteButton_6() { return static_cast<int32_t>(offsetof(UnlockStoreAnimals_t1320687291, ___serpienteButton_6)); }
+	inline Button_t4055032469 * get_serpienteButton_6() const { return ___serpienteButton_6; }
+	inline Button_t4055032469 ** get_address_of_serpienteButton_6() { return &___serpienteButton_6; }
+	inline void set_serpienteButton_6(Button_t4055032469 * value)
+	{
+		___serpienteButton_6 = value;
+		Il2CppCodeGenWriteBarrier((&___serpienteButton_6), value);
+	}
+
+	inline static int32_t get_offset_of_aveImg_7() { return static_cast<int32_t>(offsetof(UnlockStoreAnimals_t1320687291, ___aveImg_7)); }
+	inline GameObject_t1113636619 * get_aveImg_7() const { return ___aveImg_7; }
+	inline GameObject_t1113636619 ** get_address_of_aveImg_7() { return &___aveImg_7; }
+	inline void set_aveImg_7(GameObject_t1113636619 * value)
+	{
+		___aveImg_7 = value;
+		Il2CppCodeGenWriteBarrier((&___aveImg_7), value);
+	}
+
+	inline static int32_t get_offset_of_aranaImg_8() { return static_cast<int32_t>(offsetof(UnlockStoreAnimals_t1320687291, ___aranaImg_8)); }
+	inline GameObject_t1113636619 * get_aranaImg_8() const { return ___aranaImg_8; }
+	inline GameObject_t1113636619 ** get_address_of_aranaImg_8() { return &___aranaImg_8; }
+	inline void set_aranaImg_8(GameObject_t1113636619 * value)
+	{
+		___aranaImg_8 = value;
+		Il2CppCodeGenWriteBarrier((&___aranaImg_8), value);
+	}
+
+	inline static int32_t get_offset_of_serpienteImg_9() { return static_cast<int32_t>(offsetof(UnlockStoreAnimals_t1320687291, ___serpienteImg_9)); }
+	inline GameObject_t1113636619 * get_serpienteImg_9() const { return ___serpienteImg_9; }
+	inline GameObject_t1113636619 ** get_address_of_serpienteImg_9() { return &___serpienteImg_9; }
+	inline void set_serpienteImg_9(GameObject_t1113636619 * value)
+	{
+		___serpienteImg_9 = value;
+		Il2CppCodeGenWriteBarrier((&___serpienteImg_9), value);
+	}
+
+	inline static int32_t get_offset_of_aveMiniImg_10() { return static_cast<int32_t>(offsetof(UnlockStoreAnimals_t1320687291, ___aveMiniImg_10)); }
+	inline GameObject_t1113636619 * get_aveMiniImg_10() const { return ___aveMiniImg_10; }
+	inline GameObject_t1113636619 ** get_address_of_aveMiniImg_10() { return &___aveMiniImg_10; }
+	inline void set_aveMiniImg_10(GameObject_t1113636619 * value)
+	{
+		___aveMiniImg_10 = value;
+		Il2CppCodeGenWriteBarrier((&___aveMiniImg_10), value);
+	}
+
+	inline static int32_t get_offset_of_aranaMiniImg_11() { return static_cast<int32_t>(offsetof(UnlockStoreAnimals_t1320687291, ___aranaMiniImg_11)); }
+	inline GameObject_t1113636619 * get_aranaMiniImg_11() const { return ___aranaMiniImg_11; }
+	inline GameObject_t1113636619 ** get_address_of_aranaMiniImg_11() { return &___aranaMiniImg_11; }
+	inline void set_aranaMiniImg_11(GameObject_t1113636619 * value)
+	{
+		___aranaMiniImg_11 = value;
+		Il2CppCodeGenWriteBarrier((&___aranaMiniImg_11), value);
+	}
+
+	inline static int32_t get_offset_of_serpienteMiniImg_12() { return static_cast<int32_t>(offsetof(UnlockStoreAnimals_t1320687291, ___serpienteMiniImg_12)); }
+	inline GameObject_t1113636619 * get_serpienteMiniImg_12() const { return ___serpienteMiniImg_12; }
+	inline GameObject_t1113636619 ** get_address_of_serpienteMiniImg_12() { return &___serpienteMiniImg_12; }
+	inline void set_serpienteMiniImg_12(GameObject_t1113636619 * value)
+	{
+		___serpienteMiniImg_12 = value;
+		Il2CppCodeGenWriteBarrier((&___serpienteMiniImg_12), value);
+	}
+};
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+#endif // UNLOCKSTOREANIMALS_T1320687291_H
+
+
+
+
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize2300 = { sizeof (UnlockStoreAnimals_t1320687291), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable2300[9] = 
+{
+	UnlockStoreAnimals_t1320687291::get_offset_of_aveButton_4(),
+	UnlockStoreAnimals_t1320687291::get_offset_of_aranaButton_5(),
+	UnlockStoreAnimals_t1320687291::get_offset_of_serpienteButton_6(),
+	UnlockStoreAnimals_t1320687291::get_offset_of_aveImg_7(),
+	UnlockStoreAnimals_t1320687291::get_offset_of_aranaImg_8(),
+	UnlockStoreAnimals_t1320687291::get_offset_of_serpienteImg_9(),
+	UnlockStoreAnimals_t1320687291::get_offset_of_aveMiniImg_10(),
+	UnlockStoreAnimals_t1320687291::get_offset_of_aranaMiniImg_11(),
+	UnlockStoreAnimals_t1320687291::get_offset_of_serpienteMiniImg_12(),
+};
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
