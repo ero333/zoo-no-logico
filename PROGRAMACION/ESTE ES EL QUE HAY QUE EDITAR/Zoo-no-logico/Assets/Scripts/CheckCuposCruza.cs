3632120022f264a18a1e312b547dff4f4b714f77
Mateo Diaz Vera <diazveramateo@gmail.com>
2022-10-26 15:53:24 -0300

    Terminado botones extraccion y venta en Stasis y chequeo de cupos de Jaulas y Stasis en cruza nueva

---
+++ PROGRAMACION/ESTE ES EL QUE HAY QUE EDITAR/Zoo-no-logico/Assets/Scripts/CheckCuposCruza.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class CheckCuposCruza : MonoBehaviour {
+
+	int cuposJaulas;
+	int cuposStasis;
+	[SerializeField] Button buttonJaulas;
+	[SerializeField] Button buttonStasis;
+
+
+	// Use this for initialization
+	void Start () {
+		cuposJaulas = PlayerPrefs.GetInt("JaulasOcupadas");
+		cuposStasis = PlayerPrefs.GetInt("CantidadCruzasEnStasis");
+
+		if(cuposJaulas >= 20)
+        {
+			buttonJaulas.interactable = false;
+        }
+        else
+        {
+			buttonJaulas.interactable = true;
+		}
+
+		if (cuposStasis >= 10)
+		{
+			buttonStasis.interactable = false;
+		}
+		else
+		{
+			buttonStasis.interactable = true;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
--- PROGRAMACION/ESTE ES EL QUE HAY QUE EDITAR/Zoo-no-logico/Assets/Scripts/Stasis.cs
+++ PROGRAMACION/ESTE ES EL QUE HAY QUE EDITAR/Zoo-no-logico/Assets/Scripts/Stasis.cs
@@ -19,6 +19,7 @@ public class Stasis : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
 		
 	}
 
