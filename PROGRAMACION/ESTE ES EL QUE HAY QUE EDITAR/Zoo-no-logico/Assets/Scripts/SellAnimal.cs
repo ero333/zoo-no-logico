a7c0cbc3b0f11290c131b8542c7412e9657ab498
Mateo Diaz Vera <diazveramateo@gmail.com>
2022-10-26 13:02:11 -0300

    fix botones extraer y mandar a lazaro

--- PROGRAMACION/ESTE ES EL QUE HAY QUE EDITAR/Zoo-no-logico/Assets/Scripts/SellAnimal.cs
+++ PROGRAMACION/ESTE ES EL QUE HAY QUE EDITAR/Zoo-no-logico/Assets/Scripts/SellAnimal.cs
@@ -1,21 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
 
 public class SellAnimal : MonoBehaviour {
 
     int plataTotal;
     int nuevaPlata;
 
-	// Use this for initialization
-	void Start () {
-        
-	}
+    [SerializeField] GameObject smCanvas;
+    StasisManager sm;
+
+    [SerializeField] Button button;
+
+    //public int[] StasisActivas = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+    void Awake()
+    {
+        sm = smCanvas.GetComponent<StasisManager>();
+    }
+
+    // Use this for initialization
+    void Start () {
+
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
         plataTotal = PlayerPrefs.GetInt("Moneditas");
         nuevaPlata = PlayerPrefs.GetInt("plataCurrentCruza");
+
+        int pos = PlayerPrefs.GetInt("StasisPosition");
+        if (sm.StasisActivas[pos] == 1)
+        {
+            button.interactable = true;
+        } else
+        {
+            button.interactable = false;
+        }
     }
 
     public void Vender()
@@ -29,11 +53,20 @@ public class SellAnimal : MonoBehaviour {
 
     public void Sacar()
     {
-        PlayerPrefs.SetInt("indexCurrentCruza", int.Parse(PlayerPrefs.GetString("Stasis" + PlayerPrefs.GetInt("StasisPosition"))));
+        //int pos = PlayerPrefs.GetInt("StasisPosition");
+        //if (sm.StasisActivas[pos] == 1)
+        //{
+        int pos = PlayerPrefs.GetInt("StasisPosition");
+        PlayerPrefs.SetInt("indexCurrentCruza", int.Parse(PlayerPrefs.GetString("Stasis" + pos)));
 
-        PlayerPrefs.SetInt("StasisActiva" + PlayerPrefs.GetInt("StasisPosition").ToString(), 0);
-        PlayerPrefs.SetInt("CantidadCruzasEnStasis", PlayerPrefs.GetInt("CantidadCruzasEnStasis") - 1);
-        PlayerPrefs.SetString("Stasis" + PlayerPrefs.GetInt("StasisPosition").ToString(), "");
+            PlayerPrefs.SetInt("StasisActiva" + PlayerPrefs.GetInt("StasisPosition").ToString(), 0);
+            PlayerPrefs.SetInt("CantidadCruzasEnStasis", PlayerPrefs.GetInt("CantidadCruzasEnStasis") - 1);
+            PlayerPrefs.SetString("Stasis" + PlayerPrefs.GetInt("StasisPosition").ToString(), "");
+        //} else
+        //{
+        //    Debug.LogError("no se puede extraer porque no hay animal");
+        //}
+        
 
     }
 }
