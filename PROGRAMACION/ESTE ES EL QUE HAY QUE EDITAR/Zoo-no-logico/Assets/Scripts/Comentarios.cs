488d11d96e059cd083d8a6dc5478f1402ce34c8b
Delri16 <zantyyyy@gmail.com>
2022-11-12 20:20:44 -0300

    Ahora si corregido en serio antes me falto una cosa

--- PROGRAMACION/ESTE ES EL QUE HAY QUE EDITAR/Zoo-no-logico/Assets/Scripts/Comentarios.cs
+++ PROGRAMACION/ESTE ES EL QUE HAY QUE EDITAR/Zoo-no-logico/Assets/Scripts/Comentarios.cs
@@ -9,9 +9,9 @@ public class Comentarios : MonoBehaviour {
 
     private string[] Comentariosuwu = new string[] { "¡Me saqué una selfie con una ave secretaria!", "Los zorros estaban acurrucados durmiendo una siesta ¡súper tiernos!", "¡La Serpiente se tragó mi brazalete!", "¡La serpiente-cocodrilo-ave secretaria quebró un coco sólo con su pico!¡Fue asombroso!", "¡Serpiente-ave secretaria-murciélago se abalanzó contra el vidrio de su jaula apenas nos vio! Mi gorra roja lo atrajo.", "El murciélago-ave secretaria-zorro es realmente molesto, su cacareo puede volver loco a cualquiera.", "¡La araña-zorro-murciélago no hace nada más que comer!", "La cola del zorro-cocodrilo-murciélago es muy fuerte, me intimida.", "El zorro-ave secretaría-araña es realmente majestuoso, pero dicen que hace trizas a sus presas esparciendo su sangre por todas partes.", "El ave secretaría-carpincho-araña tiene unos colores tan bonitos.", "No pudimos ver bien al zorro-serpiente-ave secretaria, estaba enroscado en sí mismo.", "¡El carpincho-zorro-araña es tan tierno, pero cuando tiembla hace un ruido muy raro que lastima los oídos!", "¡El ave secretaria- murciélago-araña me saludo con tres de sus patas!", "¡El zorro-cocodrilo-ave secretaria dejó de bailar! ¡Todos los cuidadores están muy preocupados!", "El zorro-serpiente-carpincho parecía estar tratando de comunicarse con una mariposa, eso, o estaba asustado de que lo atacará.", "A pesar de que el observatorio estaba lleno, el ave secretaria-carpincho-murciélago seguía haciéndose el muerto", "La serpiente-murciélago-carpincho arrinconó a los niños y no dejaba que los cuidadores se les acercaran, parece que quería quedárselos como reemplazo de sus crías.", "¡El zorro-serpiente-murciélago estaba llorando! Parece que se estresó por la cantidad de gente que lo observaba.", "¡Hoy, el cocodrilo-carpincho-ave secretaria robó un huevo de un pájaro y se puso a incubarlo!", "¡Hoy por fin pude dar una vuelta en el lomo del cocodrilo-murciélago-carpincho!¡Fue tan divertido!",  }; //Agregar los comentarios acá
     private string[] Comentariosuwu2 = new string[] { "Unas arañas se metieron en los pantalones de un cuidador", "Una niña le dio algodón de azúcar a un cocodrilo", "¡voy a volver la próxima semana!", "¡Un carpincho estaba tomando mate!", "La serpiente - carpincho - araña es realmente aterradora, jugó con su comida y luego la despedazó frente a nosotros", "¡La Araña-carpincho-murciélago le arranco un brazo a un cuidador con sus pinzas!", "¡La Serpiente-zorro-cocodrilo se robó mi brazalete!", "¡El Zorro-serpiente-araña se enredó en su propia telaraña!", "¡El Ave secretaria-cocodrilo-araña estaba haciendo un baile súper extraño frente a su estanque!", "¡El zorro-ave secretaria-carpincho es tan lindo! Si pudiera, me lo llevaría a casa", "¡La serpiente-ave secretaria-araña engañó a los guardias y se escapó de su jaula! Por suerte la atraparon a tiempo", "¡El carpincho-zorro-murciélago enamoró a mis hijos!¡No podían dejar de hablar de lo tierno que era!", "¡Pudimos ver cómo la cola de la serpiente-murciélago-araña florecía!¡Fue un espectáculo increíble!", "¡El Zorro-cocodrilo-araña tiene mucha fuerza, el vidrio de su jaula tiene marcas de sus dientes!", "No pude ver a la araña-carpincho-cocodrilo, ¡nunca salió de su madriguera!", "La serpiente-cocodrilo-carpincho es enorme, no creí que hubiera suficientes insectos para que comiera", "¡El zorro-cocodrilo-carpincho se descontroló y le arrancó una pierna a uno de los cuidadores! Fue desagradable, pero no podía dejar de mirar.", "¡La araña-murciélago-cocodrilo está loca, estaba tratando de pelear con una roca!", "Es increíble como la serpiente-carpincho-ave secretaria se mueve usando su cola, ¡es tan rápida!", "¡La escoliosis de la serpiente-cocodrilo-araña del zoo empeoró otra vez! Espero que mejore", }; //Agregar los comentarios acá
-    private int ComentarioRandom;
-    private int ComentarioRandom2;
-    private int AvatarRandom1;
+     int ComentarioRandom;
+    int ComentarioRandom2;
+    int AvatarRandom1;
     public Text Comentario;
     public Text Comentario2;
     public Sprite avatar1;
@@ -37,7 +37,7 @@ public class Comentarios : MonoBehaviour {
         print(Comentariosuwu[ComentarioRandom]);
 
         Comentario = GameObject.FindGameObjectWithTag("ComentarioTexto").GetComponent<Text>();
-        Comentario.text = Comentariosuwu[ComentarioRandom];
+        
 
        //primeravatar = GameObject.FindGameObjectWithTag("avatar").GetComponent<Image>();
 
@@ -45,11 +45,25 @@ public class Comentarios : MonoBehaviour {
         print(Comentariosuwu2[ComentarioRandom2]);
 
         Comentario2 = GameObject.FindGameObjectWithTag("ComentarioTexto2").GetComponent<Text>();
-        Comentario2.text = Comentariosuwu2[ComentarioRandom2];
+        
 
         
 
         //segundoavatar = GameObject.FindGameObjectWithTag("avatar2").GetComponent<Image>();
+
+       
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        ComentarioRandom = PlayerPrefs.GetInt("comentarioRandom1");
+        ComentarioRandom2 = PlayerPrefs.GetInt("comentarioRandom2");
+		AvatarRandom1 = PlayerPrefs.GetInt("avatarRandom");
+
+
+        Comentario.text = Comentariosuwu[ComentarioRandom];
+        Comentario2.text = Comentariosuwu2[ComentarioRandom2];
 
         switch (AvatarRandom1)
         {
@@ -108,11 +122,4 @@ public class Comentarios : MonoBehaviour {
                 break;
         }
     }
-	
-	// Update is called once per frame
-	void Update () {
-        ComentarioRandom = PlayerPrefs.GetInt("comentarioRandom1");
-        ComentarioRandom2 = PlayerPrefs.GetInt("comentarioRandom2");
-		AvatarRandom1 = PlayerPrefs.GetInt("avatarRandom");
-	}
 }
